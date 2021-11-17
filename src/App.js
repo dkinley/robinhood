671376e68972f7d8ff5ba1a6ab821813674da1d8
Dirk Kinley <dkinley@gmail.com>
2021-11-17 13:01:40 -0800

    updates to robinhood header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     <div className="App">
       {/*Header*/}
       <div className="app__header">
-      
+        <Header />
       </div>
       {/*Body*/}
       <div className="app__body">
